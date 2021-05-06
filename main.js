3e3ab703f3b9939c3b45122d3e3542cd28bef253
Jason <kangja1996@gmail.com>
2021-05-05 22:18:12 -0400

    progress

--- main.js
+++ main.js
@@ -23,18 +23,34 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 // });
 
 // filter
-let canDrink = [];
-for (let i = 0; i < ages.length; i++) {
-  if (ages[i] > 15) {
-    canDrink.push(ages[i]);
-  }
-}
+// let canDrink = [];
+// for (let i = 0; i < ages.length; i++) {
+//   if (ages[i] > 15) {
+//     canDrink.push(ages[i]);
+//   }
+// }
 
-console.log(canDrink);
-// ages.filter(function (age) {
-//   console.log(age > 15);
+// const canDrink = ages.filter(function (age) {
+//   if (age >= 21) {
+//     return true;
+//   }
+// })
+
+// const canDrink = ages.filter((age) => age >= 21);
+
+// console.log(canDrink);
+
+// const retailCompanies = companies.filter(function (company) {
+//   if (company.category === "Retail") {
+//     return true;
+//   }
 // });
 
+const retailCompanies = companies.filter(
+  (company) => company.category === "Retail"
+);
+
+console.log(retailCompanies);
 // map
 
 // sort
