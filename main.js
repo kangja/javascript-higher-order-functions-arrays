c7ca8d5fbdd2cd30e83cf5101c67ee46f5d594d6
Jason <kangja1996@gmail.com>
2021-05-07 23:21:46 -0400

    progress

--- main.js
+++ main.js
@@ -46,12 +46,45 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 //   }
 // });
 
-const retailCompanies = companies.filter(
-  (company) => company.category === "Retail"
+// const retailCompanies = companies.filter(
+//   (company) => company.category === "Retail"
+// );
+
+// console.log(retailCompanies);
+
+// const eightyCompanies = companies.filter(
+//   (company) => company.start >= 1980 && company.start < 1990
+// );
+
+// console.log(eightyCompanies);
+
+// companies that lasted 10 years or more
+// const lastedTenYears = companies.filter(
+//   (company) => company.end - company.start >= 10
+// );
+
+// console.log(lastedTenYears);
+
+// map
+// const companyNames = companies.map(function (company) {
+//   return company.name;
+// });
+
+// const test = companies.map(function (company) {
+//   return 1;
+// });
+
+// console.log(test);
+
+// const testMap = companies.map(function (company) {
+//   return `${company.name} [${company.start} - ${company.end}]`;
+// });
+
+const testMap = companies.map(
+  (company) => `${company.name} [${company.start} - ${company.end}]`
 );
 
-console.log(retailCompanies);
-// map
+console.log(testMap);
 
 // sort
 
